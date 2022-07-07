c35a36646a09ed236ab44e1e5448e68208e8d8c2
Tyler Crammond <tylerchc92@gmail.com>
2022-07-07 22:09:54 +0100

    Upgrade node-fetch

--- api/cat.js
+++ api/cat.js
@@ -1,20 +1,21 @@
-const fetch = require('node-fetch')
+import fetch from 'node-fetch'
 
 const pexelsApiKey = process.env.PEXELS_API_KEY
 
-module.exports = (req, res) => {
+export default async function handler(request, response) {
     const imageNumber = Math.floor(Math.random() * 1000) + 1
 
-    fetch(`https://api.pexels.com/v1/search?query=cat&per_page=1&page=${imageNumber}`, {
+    const res = await fetch(`https://api.pexels.com/v1/search?query=cat&per_page=1&page=${imageNumber}`, {
         headers: {
             Authorization: pexelsApiKey
         }
     })
-        .then(res => res.json())
-        .then(json => res.json({
-            imageUrl: `${json.photos[0].src.original}?auto=compress&cs=tinysrgb&dpr=2&h=300`,
-            pexelsUrl: json.photos[0].url,
-            photographer: json.photos[0].photographer,
-            photographerUrl: json.photos[0].photographer_url,
-        }))
+    const data = await res.json()
+
+    return response.status(200).json({
+        imageUrl: `${data.photos[0].src.original}?auto=compress&cs=tinysrgb&dpr=2&h=300`,
+        pexelsUrl: data.photos[0].url,
+        photographer: data.photos[0].photographer,
+        photographerUrl: data.photos[0].photographer_url,
+    })
 }
