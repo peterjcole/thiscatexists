188c5d1eb72829161099bf13e879876f6d7bb2f3
Tyler <tylerchc92@gmail.com>
2022-07-08 14:41:27 +0100

    Dark spring clean (#4)
    * Upgrade node-fetch
    
    * Refresh formatting
    
    * Refresh style
    
    * Add dark mode
    
    * Adjust font
    
    * Update public/index.css
    
    Co-authored-by: Peter Cole <11048765+peterjcole@users.noreply.github.com>

--- public/index.js
+++ public/index.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('/api/cat').then(res => res.json()).then(json => {
-        const image = new Image()
-        image.id = 'cat-pic'
-        image.className = 'cat-pic'
-        image.alt = 'an adorable cat'
-        image.onload = () => {
-            const [photoLink, authorLink] = document.querySelectorAll('#cat-tion a')
-            photoLink.setAttribute('href', json.pexelsUrl)
-            authorLink.setAttribute('href', json.photographerUrl)
-            authorLink.innerText = json.photographer
+	fetch('/api/cat').then(res => res.json()).then(json => {
+		const image = new Image()
+		image.id = 'cat-pic'
+		image.className = 'cat-pic'
+		image.alt = 'an adorable cat'
+		image.onload = () => {
+			const [photoLink, authorLink] = document.querySelectorAll('#cat-tion a')
+			photoLink.setAttribute('href', json.pexelsUrl)
+			authorLink.setAttribute('href', json.photographerUrl)
+			authorLink.innerText = json.photographer
 
-            document.querySelector('.cat').classList.remove('hidden')
-            document.querySelector('.loading').classList.add('hidden')
-        }
-        image.src = json.imageUrl
+			document.querySelector('.cat').classList.remove('hidden')
+			document.querySelector('.loading').classList.add('hidden')
+		}
+		image.src = json.imageUrl
 
-        document.querySelector('figure').prepend(image)
-    })
+		document.querySelector('figure').prepend(image)
+	})
 });
